a86df11281c0dec99a49fc171aece97ae2cfc68b
lilei <1543185970@qq.com>
2019-11-15 18:58:56 +0800

    feat:完成分页功能

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -5,29 +5,41 @@
       <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
       <el-table-column prop="mobile" label="电话"></el-table-column>
       <el-table-column prop="mg_state" label="用户状态"></el-table-column>
-      <el-table-column prop="" label="操作"></el-table-column>
+      <el-table-column prop label="操作"></el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="total"
+      :page-size="pageSize"
+      :current-page="curPage"
+      @current-change="changePage"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
 export default {
   created() {
-    this.getUserList();
+    this.getUserList(1);
   },
   data() {
     return {
-      userList: []
+      userList: [],
+      pageSize: 3,
+      curPage: 1,
+      total: 10
     };
   },
 
   methods: {
-    getUserList() {
+    getUserList(num) {
       this.$http
         .get("/users", {
           params: {
-            pagenum: 1,
-            pagesize: 3
+            pagenum: num,
+            pagesize: this.pageSize
           },
           headers: {
             Authorization: localStorage.getItem("token")
@@ -36,10 +48,15 @@ export default {
         .then(res => {
           const { data, meta } = res.data;
           if (meta.status === 200) {
-              console.log(data)
+            console.log(data);
             this.userList = data.users;
+            this.total = data.total;
           }
         });
+    },
+    // ui库提供的分页点击页码事件
+    changePage(curPage) {
+      this.getUserList(curPage);
     }
   }
 };
