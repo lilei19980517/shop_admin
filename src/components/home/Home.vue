35b47c19f851174f55fa7af5ba3931c6af4bfd17
lilei <1543185970@qq.com>
2019-11-15 13:25:44 +0800

    feat:完成home页面的退出功能和基本布局样式

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,13 +1,87 @@
 <template>
-    <div>这是首页</div>
+  <div class="home-root">
+    <el-container>
+      <el-header>
+        <h1>电商后台管理</h1>
+        <div>
+          <span>欢迎您！后台管理员</span>
+          <a href="#" @click.prevent="quit">退出</a>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-  export default {
-      
+export default {
+  methods: {
+    quit() {
+      // 弹出确认对话框
+      this.$confirm("您确认退出吗？","提示",{
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 点击确认清除locastroaga
+        localStorage.removeItem('token')
+      // 跳回登陆页面
+        this.$router.push('/login')
+      })          
+    }
   }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.home-root {
+  height: 100%;
+  .el-container {
+    height: 100%;
+    .el-header {
+      background-color: #a8bfd3;
+      color: #333;
+      text-align: center;
+      line-height: 100%;
+      display: flex;
+      justify-content: space-between;
+      div {
+        width: 300px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        span {
+          font-size: 20px;
+        }
+        a {
+          font-size: 18px;
+          font-weight: 610;
+          text-decoration: none;
+        }
+      }
+      h1 {
+        flex: 1;
+        align-self: center;
+        margin-right: -300px;
+      }
+    }
 
+    .el-aside {
+      background-color: #4f555a;
+      color: #333;
+      text-align: center;
+      line-height: 200px;
+    }
+
+    .el-main {
+      background-color: #e9eef3;
+      color: #333;
+      text-align: center;
+      line-height: 160px;
+    }
+  }
+}
 </style>
